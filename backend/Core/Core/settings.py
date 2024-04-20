f8a5b018f9d3b85a7dd365c44e75ffd530a64d88
Nitish Singh <fgrreloadedprogrammer@gmail.com>
2024-04-20 16:53:07 +0530

    fix bugs

--- backend/Core/Core/settings.py
+++ backend/Core/Core/settings.py
@@ -22,9 +22,6 @@ cloudinary.config(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# secret = config('SECRET_KEY')
-# print(secret)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
