9b6d2f30139526f18fc55462ddc98125ef765f54
Nitish Singh <fgrreloadedprogrammer@gmail.com>
2024-04-20 16:50:34 +0530

    fix: solve env problem

--- backend/envgla/Scripts/activate.bat
+++ backend/envgla/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=G:\GLA-OLX\gla_olx\backend\envgla
+set VIRTUAL_ENV=N:\Programming\Repositories\Gla_OLX\backend\envgla
 
 if not defined PROMPT set PROMPT=$P$G
 
