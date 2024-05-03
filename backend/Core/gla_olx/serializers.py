7e979806445d6e370ddd41b81f99c4216fb29fd5
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-05-03 23:51:45 +0530

    update: product id auto generated by masterblaster shashank

--- backend/Core/gla_olx/serializers.py
+++ backend/Core/gla_olx/serializers.py
@@ -4,7 +4,7 @@ from .models import *
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductDetails
-        fields = ['id','seller_id', 'product_title', 'product_price', 'product_desc', 'product_category', 'product_subcategory', 'seller_name', 'seller_picture', 'product_status' ,'created_date']
+        fields = ['product_id','seller_id', 'product_title', 'product_price', 'product_desc', 'product_category', 'product_subcategory', 'seller_name', 'seller_picture', 'product_status' ,'created_date']
         read_only_fields = ['proimg1', 'proimg2', 'proimg3', 'proimg4','proimg5']
 
 
